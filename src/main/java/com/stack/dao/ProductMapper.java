ccc51da0238672416296481e1a9b39397360a877
woaidtt <502711108@qq.com>
2017-05-25 16:47:55 +0800

    first commit

---
+++ src/main/java/com/stack/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.stack.dao;
+
+import com.stack.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
